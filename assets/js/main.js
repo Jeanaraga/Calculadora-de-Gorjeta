65fd67a83ff05cf0cf06c7c7b1c100bd266b1a40
Jean Pedro <jeanjesuspedrobook@gmail.com>
2022-04-14 21:32:30 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,42 @@
-const gorButtons = document.querySelectorAll('.btn-value')
+const formulario = document.querySelector('.form-pre-result');
+
+const buttonsValue = document.querySelectorAll('.btn-value');
 
 function handleClick(event) {
     event.preventDefault();
-    gorButtons.forEach((item) => {
-        item.classList.remove('clicado')
-    })
-    event.currentTarget.classList.add('clicado')
+    buttonsValue.forEach((item) => {
+        item.classList.remove('clicado');
+    });
+    event.currentTarget.classList.add('clicado');
 }
 
-gorButtons.forEach((item) => {
+buttonsValue.forEach((item) => {
     item.addEventListener('click', handleClick)
 })
 
-let conta = document.querySelector('form');
+formulario.addEventListener('submit', function(e) {
+    const firstInput = document.querySelector('.conta');
+    const primeiroValor = firstInput.value;
+    const threeInput = document.querySelector('#num-people');
+    const segundoValor = threeInput.value;
+    const twoInput = document.querySelector('.clicado')
+    const terceiroValor = twoInput.value;
+    const porcentagem = (terceiroValor / 100) * primeiroValor;
+    const resultadoPessoa = porcentagem / segundoValor;
+    const totalPessoa = resultadoPessoa + (primeiroValor / segundoValor);
+    const gorjetaPessoa = document.querySelector('.tip-amount');
+    const result1 = resultadoPessoa.toFixed(2);
+    gorjetaPessoa.innerHTML = "$" + result1;
+    const total = document.querySelector('.total');
+    const result2 = totalPessoa.toFixed(2);
+    total.innerHTML = "$" + result2;
 
-conta.addEventListener('submit', function (e) {
-    let aConta = document.querySelector('.conta');
-    let gorjeta = document.querySelector('.clicado');
-    let valorGor = gorjeta.innerHTML - "%";
-    let numPessoas = document.getElementById('num-people')
-    let resultado = document.querySelector('.tip-amount');
-    resultado.innerHTML = '$' + aConta.value / numPessoas.value - gorjeta;
+    console.log("O numero teste deu " + resultadoPessoa)
     e.preventDefault();
 })
 
-let gorjeta = document.querySelector('.clicado');
-    let valorGor = gorjeta.innerHTML - "%";
+const btnReset = document.querySelector('.button-reset button');
 
-    console.log(valorGor)
+btnReset.addEventListener('click', function() {
+    location.reload();
+})
